6e9f1324d75c387a94bd8f40cc5878861ea38a37
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-15 18:39:26 -0300

    fix: ajustando animação para hover

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -5,7 +5,7 @@ export default function Link(props: { linkName: string }): ReactElement {
   return (
     <a
       href=""
-      className="w-fit h-5 text-esmerald last:mask-conic-to-neutral-50 mt-5 flex flex-row items-center justify-between gap-x-1"
+      className="font-hunnin w-fit h-5 px-2 py-4 rounded-2xl text-esmerald last:mask-conic-to-neutral-50 mt-5 flex flex-row items-center justify-between gap-x-1 hover:text-white hover:bg-esmerald transition-colors delay-100"
     >
       <p className="link">{props.linkName}</p>
       <FaArrowTrendUp className="w-fit h-fit" />
--- src/components/Presentation/Banner/Banner.tsx
+++ src/components/Presentation/Banner/Banner.tsx
@@ -1,6 +1,7 @@
 import type { BannerProps } from "../../../types/BannerProps";
 import type { ReactElement } from "react";
 import BannerTitle from "./BannerTitle";
+import Link from "src/components/Link";
 
 export default function Banner({
   bannerTitle,
@@ -11,9 +12,7 @@ export default function Banner({
     <div className="padding-h-content w-1/2 h-4/7 flex flex-col items-start justify-between">
       <BannerTitle title={bannerTitle} />
       <p className="text-white w-5/6 font-hunnin text-sm">{description}</p>
-      <button className="text-white font-hunnin hover:cursor-pointer hover:border-2 hover:border-esmerald p-2 hover:rounded-lg">
-        {buttonContent}
-      </button>
+      <Link linkName={buttonContent} />
     </div>
   );
 }
