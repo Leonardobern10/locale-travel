8aadc0be72ba26768ed0cd1ecb839e8a823cf2ff
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-15 21:39:30 -0300

    feat: novo component 'we offer'

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import LocaleWay from "./components/LocaleWay/LocaleWay";
 import OurValues from "./components/OurValue/OurValues";
 import Presentation from "./components/Presentation/Presentation";
-import { ImAirplane } from "react-icons/im";
+import WeOffer from "./components/WeOffer/WeOffer";
 
 function App() {
-  const cursor = ImAirplane;
   return (
-    <div
-      style={{ cursor: `${cursor}` }}
-      className="flex flex-col items-center justify-between"
-    >
+    <div className="flex flex-col items-center justify-between">
       <Presentation />
       <LocaleWay />
       <OurValues />
+      <WeOffer />
     </div>
   );
 }
---
+++ src/components/WeOffer/WeOffer.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+export default function WeOffer(): ReactElement {
+  return (
+    <div className="h-120 bg-green-200">
+      <div>we offer</div>
+    </div>
+  );
+}
