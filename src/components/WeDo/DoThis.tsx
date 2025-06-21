b3185eba588e6d4ae4c267700d336025c0507863
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-21 16:29:03 -0300

    feat: definindo as props a serem recebidas

--- src/components/WeDo/DoThis.tsx
+++ src/components/WeDo/DoThis.tsx
@@ -1,11 +1,19 @@
+import type { DoThisProps } from "src/types/DoThisProps";
 import type { ReactElement } from "react";
 
-export default function DoThis(): ReactElement {
+export default function DoThis({
+  icon,
+  subtitle,
+  text,
+}: DoThisProps): ReactElement {
+  const Icon = icon;
   return (
     <article className="border-2 flex flex-col items-start justify-evenly gap-5 p-8">
-      <div className="w-10 h-10 border-2">icon</div>
-      <h5>subtitulo</h5>
-      <p>texto</p>
+      <div className="w-10 h-10 border-2">
+        <Icon />
+      </div>
+      <h5>{subtitle}</h5>
+      <p>{text}</p>
     </article>
   );
 }
