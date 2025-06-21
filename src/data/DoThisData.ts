2bb2a3cbace43a18e212f60fd8214bcbbff7cb48
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-21 16:23:57 -0300

    new: adicionando os dados para DoThis.tsx

---
+++ src/data/DoThisData.ts
@@ -0,0 +1,45 @@
+import { BsBuildings } from "react-icons/bs";
+import { LuBriefcaseBusiness } from "react-icons/lu";
+import { IoMdFootball } from "react-icons/io";
+import { GiDualityMask } from "react-icons/gi";
+import { TbUsersGroup } from "react-icons/tb";
+import { MdOutlineAirplaneTicket } from "react-icons/md";
+
+export const doThisData = [
+  {
+    id: 0,
+    icon: BsBuildings,
+    subtitle: "Unique Event Travel",
+    text: "Locale Travel provides a full spectrum of  Event Travel Management Services.  We can provide the hotel rooms you need.  We can provide the transfers you need from the airport to the hotel.",
+  },
+  {
+    id: 1,
+    icon: LuBriefcaseBusiness,
+    subtitle: "Corporate",
+    text: "At Locale Travel, our job is to guide you through this fast-paced environment, ensuring your travel program is running smoothly and efficiently while your travellers receive the best in customer service, technology and security.",
+  },
+  {
+    id: 2,
+    icon: IoMdFootball,
+    subtitle: "Sports",
+    text: "Sport isn’t confined to office hours – and neither are we. It's inevitable that most of the last minute glitches that occur tend to happen out of business hours, when many sports travel management companies have closed their doors and gone home.",
+  },
+  {
+    id: 3,
+    icon: GiDualityMask,
+    subtitle: "Entertainment Travel",
+    text: "Highly experienced staff that understand the flexibility and organisation that’s needed. Over three decades of experience with touring parties. Close relationships with the travel suppliers you use. Exceptional service and benefits when you need it now. Not later.",
+  },
+  {
+    id: 4,
+    icon: TbUsersGroup,
+    subtitle: "Group & Conference Travel",
+    text: "Your successful group travel and conference is created from a mix of the right people, resources and know-how. Take advantage of our unique group travel & conference technology tools. ",
+  },
+  {
+    id: 5,
+    icon: MdOutlineAirplaneTicket,
+    subtitle: "Leisure",
+    text: "You can be confident your leisure and holiday travel will be a unique and well crafted 'event' - because Locale Travel will apply the quality and personal attention and experience we apply to all our travel solutions.",
+  },
+];
