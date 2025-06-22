f5ffe5efe61045595264f39c18cd0033b83c5926
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-21 23:00:33 -0300

    new: Novo componente ContactUs.tsx

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -6,23 +6,29 @@ import Input from "./Input";
 
 export default function ContactUs(): ReactElement {
   return (
-    <section>
-      <div>
-        <img src={image} alt="" />
+    <section className="w-screen h-5/6 flex flex-row font-hunnin">
+      <div className="w-1/2">
+        <img className="w-full h-full" src={image} alt="" />
       </div>
-      <div>
-        <div>
-          <TitleSectionColored
-            title="Contact Us"
-            center={true}
-            padding={true}
-          />
-          <p>Fill out the form below.</p>
-          <form action="">
+      <div className="w-1/2 padding-h-content bg-esmerald">
+        <div className="flex flex-col gap-y-10">
+          <div className="">
+            <TitleSectionColored
+              white={true}
+              title="ContactUs"
+              center={true}
+              padding={true}
+            />
+            <p>Fill out the form below.</p>
+          </div>
+          <form className="flex flex-col gap-y-8" action="">
             <Input placeholder="Full name" email={false} />
             <Input placeholder="Email address" email={true} />
             <Input placeholder="Subject" email={false} />
-            <textarea placeholder="Message" />
+            <textarea
+              className="w-full h-50 border border-white/50 text-neutral-200 px-4 py-2 rounded-xl placeholder:text-neutral-600/50 focus:text-white focus:bg-white/50 focus:outline-none"
+              placeholder="Message"
+            />
             <ButtonDefault
               buttonName="Submit"
               onClick={() => console.log("Ola")}
