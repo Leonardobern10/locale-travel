9a8ce3d4c3203537158460fe94da10cd09456231
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-22 11:17:33 -0300

    new: Dados de contato armazenados para serem acessados e exibidos por Footer.tsx

---
+++ src/data/FooterData.ts
@@ -0,0 +1,27 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { TiSocialSkype } from "react-icons/ti";
+import { FaSquareBehance } from "react-icons/fa6";
+
+export const footerData = [
+  {
+    index: 0,
+    title: "Head Office",
+    text: "Suite 5, Level 2, 2 Grosvenor St, Bondi Junction NSW 2022",
+  },
+  { index: 1, title: "Email contact", text: "hello@localetravel.com.au" },
+  {
+    index: 2,
+    title: "Contact",
+    text: "+1300 811 033 \n+61 7 3059 6130  (from outside Australia)",
+  },
+];
+
+export const footerIcons = [
+  { index: 0, icon: FaFacebookSquare },
+  { index: 1, icon: FaSquareXTwitter },
+  { index: 2, icon: FaYoutube },
+  { index: 3, icon: TiSocialSkype },
+  { index: 4, icon: FaSquareBehance },
+];
