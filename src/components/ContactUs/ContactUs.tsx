42a7b396c89a49c252be904535f24063cd01d89a
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-26 21:36:27 -0300

    fix: melhorando acessibilidade

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -10,7 +10,11 @@ export default function ContactUs(): ReactElement {
   return (
     <section className="w-screen h-5/6 flex flex-row font-hunnin">
       <div className="w-1/2">
-        <img className="w-full h-full" src={image} alt="" />
+        <img
+          className="w-full h-full"
+          src={image}
+          alt="imagem de uma mão segurando um celular"
+        />
       </div>
       <div className="w-1/2 padding-h-content bg-esmerald">
         <div className="flex flex-col gap-y-10">
@@ -24,17 +28,50 @@ export default function ContactUs(): ReactElement {
             />
             <Subtitle white={true} title={titlesText.subtitle} />
           </div>
-          <form className="flex flex-col gap-y-8" action="">
-            <Input placeholder={placeholders.namePlaceholder} email={false} />
-            <Input placeholder={placeholders.emailPlaceholder} email={true} />
-            <Input
-              placeholder={placeholders.subjectPlaceholder}
-              email={false}
-            />
-            <textarea
-              className="w-full h-50 border border-white/50 text-neutral-200 px-4 py-2 rounded-xl placeholder:text-neutral-600/50 focus:text-white focus:bg-white/50 focus:outline-none"
-              placeholder={placeholders.messagePlaceholder}
-            />
+          <form className="flex flex-col gap-y-4 pb-4" action="">
+            <div className="flex flex-col gap-y-2">
+              <label htmlFor="name" className="text-neutral-200">
+                Nome
+              </label>
+              <Input
+                id="name"
+                placeholder={placeholders.namePlaceholder}
+                email={false}
+              />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <label htmlFor="email" className="text-neutral-200">
+                Email
+              </label>
+              <Input
+                id="email"
+                placeholder={placeholders.emailPlaceholder}
+                email={true}
+              />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <label htmlFor="subject" className="text-neutral-200">
+                Assunto
+              </label>
+              <Input
+                id="subject"
+                placeholder={placeholders.subjectPlaceholder}
+                email={false}
+              />
+            </div>
+
+            <div className="flex flex-col gap-y-2">
+              <label htmlFor="message" className="text-neutral-200">
+                Mensagem{" "}
+              </label>
+              <textarea
+                id="message"
+                className="w-full h-50 border border-white/50 text-neutral-200 px-4 py-2 rounded-xl placeholder:text-neutral-600/50 focus:text-white focus:bg-white/50 focus:outline-none"
+                placeholder={placeholders.messagePlaceholder}
+              />
+            </div>
             <ButtonDefault
               buttonName="Enviar"
               onClick={() => console.log("Ola")}
--- src/components/ContactUs/Input.tsx
+++ src/components/ContactUs/Input.tsx
@@ -3,9 +3,12 @@ import type { ReactElement } from "react";
 export default function Input(props: {
   email: boolean;
   placeholder: string;
+  id: string;
 }): ReactElement {
   return (
     <input
+      id={props.id}
+      required
       className="w-full h-13 border border-white/50 text-neutral-200 px-4 py-2 rounded-xl placeholder:text-neutral-600/50 focus:text-white focus:bg-white/50 focus:outline-none"
       type={props.email ? "email" : "text"}
       placeholder={props.placeholder}
