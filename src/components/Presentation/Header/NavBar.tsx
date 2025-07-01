9d7c8a2ee879147694ee54a83fd95d06e139abc9
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-30 23:11:56 -0300

    fix: exibindo a lista de navegação apenas quando a largura da tela for menor que 575px

--- src/components/Presentation/Header/NavBar.tsx
+++ src/components/Presentation/Header/NavBar.tsx
@@ -2,21 +2,24 @@ import type { ReactElement } from "react";
 import ButtonHeader from "../../ButtonHeader";
 import { iconsButton } from "@data/ButtonIcons";
 import type { NavBarProps } from "src/types/NavBarProps";
+import useWidth from "src/hooks/useWidth";
 
 export default function NavBar({ items }: NavBarProps): ReactElement {
   return (
-    <nav className="w-fit flex flex-row items-center justify-evenly gap-x-8">
-      <ul className="flex flex-row items-center justify-evenly gap-x-8 font-hunnin ">
-        {items.map((el, index) => (
-          <li
-            className="text-md font-bold text-white text-shadow-2xs text-shadow-black hover:cursor-pointer hover:text-shadow-xs hover:text-shadow-esmerald"
-            key={index}
-          >
-            {el}
-          </li>
-        ))}
-      </ul>
-      <div className="flex flex-row justify-between gap-x-3">
+    <nav className="w-fit flex flex-row items-center justify-between md:justify-evenly gap-x-8">
+      {useWidth() > 575 && (
+        <ul className="flex flex-row items-center justify-evenly gap-x-8 font-hunnin ">
+          {items.map((el, index) => (
+            <li
+              className="text-md font-bold text-white text-shadow-2xs text-shadow-black hover:cursor-pointer hover:text-shadow-xs hover:text-shadow-esmerald"
+              key={index}
+            >
+              {el}
+            </li>
+          ))}
+        </ul>
+      )}
+      <div className="flex flex-row items-center justify-between gap-x-3">
         {iconsButton.map((el) => (
           <ButtonHeader icon={el} />
         ))}
