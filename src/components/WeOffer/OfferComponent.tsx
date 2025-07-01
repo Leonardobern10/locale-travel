0110eda9c4c4cd413e469044b3412354692ff2c9
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-07-01 19:40:58 -0300

    fix: ajustando tamenho do componente

--- src/components/WeOffer/OfferComponent.tsx
+++ src/components/WeOffer/OfferComponent.tsx
@@ -2,6 +2,7 @@ import type { OfferDataProps } from "src/types/OfferDataProps";
 import { useRef, type ReactElement } from "react";
 import ButtonDefault from "../ButtonDefault";
 import { useScroll } from "src/animation/scroll";
+import useWidth from "src/hooks/useWidth";
 
 export default function OfferComponent({
   index,
@@ -19,7 +20,7 @@ export default function OfferComponent({
       ref={containerRef}
       className={`relative ${
         i ? "self-end" : "self-start"
-      } w-3/5 min-h-[300px]`} // 🔧 garantia de altura visível
+      } w-[90%] md:w-3/5 md:min-h-[300px]`} // 🔧 garantia de altura visível
     >
       <img
         className="w-full h-full object-cover"
@@ -29,21 +30,23 @@ export default function OfferComponent({
       <div
         className={`absolute inset-0 ${
           i ? "right-4/6" : "left-4/8"
-        } bg-emerald-500/40 w-1/2 shadow-offer`}
+        } bg-emerald-500/40 md:w-1/2 shadow-offer`}
       />
       <div
-        className={`absolute bottom-20 ${
+        className={`absolute bottom-3 md:bottom-20 ${
           i ? "left-20" : "right-20"
-        } flex flex-col gap-y-4`}
+        } flex flex-col items-center md:gap-y-4`}
       >
-        <h4 className="text-5xl text-white font-chonburi font-bold">
+        <h4 className="md:text-5xl text-white font-chonburi font-bold">
           {offerTitle}
         </h4>
-        <ButtonDefault
-          black={false}
-          buttonName={linkName}
-          onClick={() => console.log("ola")}
-        />
+        {useWidth() > 575 && (
+          <ButtonDefault
+            black={false}
+            buttonName={linkName}
+            onClick={() => console.log("ola")}
+          />
+        )}
       </div>
     </div>
   );
