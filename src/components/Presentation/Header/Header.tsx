51b57fa7033197d3acc3d3e8ab9567ed6054c870
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-30 23:18:53 -0300

    fix: ajustando espaçamento horizontal para dispositivos móveis

--- src/components/Presentation/Header/Header.tsx
+++ src/components/Presentation/Header/Header.tsx
@@ -6,18 +6,17 @@ import { useScroll } from "src/animation/scroll";
 
 export default function Header(): ReactElement {
   const headerRef = useRef<HTMLDivElement>(null);
-
   useScroll(headerRef, 1.5, 0, -50);
 
   return (
     <header
       ref={headerRef}
-      className="padding-h-content w-full h-16 md:h-20 py-1 flex flex-col md:flex-row items-center justify-between gap-y-2 md:gap-y-0"
+      className="md:padding-h-content w-full h-16 md:h-20 px-4 py-1 flex flex-row items-center justify-between gap-y-2 "
     >
-      <div className="w-28 h-12 md:w-25 md:h-15 flex-shrink-0 flex items-center justify-center">
+      <div className="w-28 h-12 md:w-fit md:h-15 flex-shrink-0 flex items-center justify-center">
         <img src={logo} alt="Logo" className="w-full h-full object-contain" />
       </div>
-      <div className="w-full md:w-auto flex justify-center md:justify-end">
+      <div className="w-fit md:w-full flex justify-center md:justify-end">
         <NavBar items={items} />
       </div>
     </header>
