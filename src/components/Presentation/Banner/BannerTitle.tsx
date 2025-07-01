9eafcb5a4041eb93db97d55a03a83dd165bf7ec3
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-06-30 23:16:41 -0300

    fix: ajustando tamanho e largura para dispositivos moveis

--- src/components/Presentation/Banner/BannerTitle.tsx
+++ src/components/Presentation/Banner/BannerTitle.tsx
@@ -4,7 +4,7 @@ import type { ReactElement } from "react";
 export default function BannerTitle({ title }: BannerTitleProps): ReactElement {
   const [firstWord, ...rest] = title.split(" ");
   return (
-    <h1 className="text-6xl text-white w-full h-45 tracking-widest font-bold font-ranchers">
+    <h1 className="text-4xl md:text-6xl text-white w-fit  md:w-full h-45 tracking-widest font-bold font-ranchers">
       <span className="text-esmerald">{firstWord}</span> {rest.join(" ")}
     </h1>
   );
