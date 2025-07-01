07b13dbec658412618f950231b73d6773e57bedc
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-07-01 20:29:34 -0300

    update: inserindo novos icones

--- src/data/LocalWayServiceData.ts
+++ src/data/LocalWayServiceData.ts
@@ -3,6 +3,9 @@ import service1 from "@images/travel_tech.jpg";
 import service2 from "@images/concierge_travel.png";
 import service3 from "@images/spend_smarter.png";
 import { BsBuildings } from "react-icons/bs";
+import { PiAirplaneTakeoffBold } from "react-icons/pi";
+import { BsPersonFillGear } from "react-icons/bs";
+import { RiMoneyDollarBoxLine } from "react-icons/ri";
 
 export const LocaleWayServiceData: LocalWayServiceProps = {
   title: "Nossa maneira",
@@ -15,7 +18,7 @@ export const LocaleWayServiceData: LocalWayServiceProps = {
       description:
         "Com sistemas inteligentes, automação e experiências personalizadas, conectamos você ao mundo com mais rapidez, segurança e conforto.",
       img: service1,
-      icon: BsBuildings,
+      icon: PiAirplaneTakeoffBold,
     },
     {
       index: 1,
@@ -23,7 +26,7 @@ export const LocaleWayServiceData: LocalWayServiceProps = {
       description:
         "Cada viajante é único — e sua jornada também deve ser. Por isso, oferecemos um serviço totalmente personalizado, pensado nos seus interesses, preferências e estilo de vida.a",
       img: service2,
-      icon: BsBuildings,
+      icon: BsPersonFillGear,
     },
     {
       index: 2,
@@ -31,7 +34,7 @@ export const LocaleWayServiceData: LocalWayServiceProps = {
       description:
         "Viajar bem não precisa significar gastar mais. Com nossas ferramentas inteligentes e parcerias estratégicas, ajudamos você a maximizar seu orçamento, encontrando as melhores ofertas sem abrir mão da qualidade e do conforto.",
       img: service3,
-      icon: BsBuildings,
+      icon: RiMoneyDollarBoxLine,
     },
   ],
 };
