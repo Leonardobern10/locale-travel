45bcd0caf3fdbf3f46d1927ff5828454eff1cac3
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-07-02 11:30:22 -0300

    add: "Adicionando componente TextDefault.tsx para textos"

--- src/components/OurValue/ParagraphValue.tsx
+++ src/components/OurValue/ParagraphValue.tsx
@@ -1,4 +1,5 @@
 import type { ReactElement } from "react";
+import TextDefault from "../TextDefault";
 
 export default function ParagraphValue(props: {
   paragraphs: Array<string>;
@@ -6,7 +7,7 @@ export default function ParagraphValue(props: {
   return (
     <div className="flex flex-col gap-y-5 tracking-wide">
       {props.paragraphs.map((el, index) => (
-        <p key={index}>{el}</p>
+        <TextDefault text={el} key={index} />
       ))}
     </div>
   );
