d3aea9b1b812a3f0810d0aa8c6ea8ce22ad87e6d
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-07-02 11:22:38 -0300

    new: Criação de novo componente padrao para formatacao de textos

---
+++ src/components/TextDefault.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+export default function TextDefault(props: { text: string }): ReactElement {
+  return (
+    <p className="text-neutral-500 text-sm md:text-lg font-hunnin">
+      {props.text}
+    </p>
+  );
+}
