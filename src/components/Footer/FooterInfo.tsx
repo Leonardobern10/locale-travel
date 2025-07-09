e7729cb1736c72e290cb7c737b2f1b54b7f1c2ee
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-07-09 19:04:39 -0300

    update: inserindo data-testid

--- src/components/Footer/FooterInfo.tsx
+++ src/components/Footer/FooterInfo.tsx
@@ -6,7 +6,11 @@ export default function FooterInfo(props: {
 }): ReactElement {
      return (
           <div className="w-5/6 md:w-1/5 text-center font-hunnin">
-               <p className="font-bold text-neutral-300">{props.title}</p>
+               <p
+                    data-testid="footer-info-title"
+                    className="font-bold text-neutral-300">
+                    {props.title}
+               </p>
                {props.content.map((el, index) => (
                     <p key={index} className="text-neutral-500">
                          {el}
