751a1acfa9fbc1560111e4dd7cb65fe294f3072c
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-07-10 15:59:27 -0300

    fix: removendo border

--- src/components/Presentation/Banner/BannerTitle.tsx
+++ src/components/Presentation/Banner/BannerTitle.tsx
@@ -4,7 +4,7 @@ import type { ReactElement } from 'react';
 export default function BannerTitle({ title }: BannerTitleProps): ReactElement {
      const [firstWord, ...rest] = title.split(' ');
      return (
-          <h1 className="border-2 text-4xl sm:text-5xl lg:text-5xl text-white w-fit md:w-2/3 lg:w-5/6 xl:w-5/8 h-45 tracking-widest font-bold font-ranchers sm:text-left sm:leading-15 text-shadow-2xs text-shadow-black">
+          <h1 className="text-4xl sm:text-5xl lg:text-5xl text-white w-fit md:w-2/3 lg:w-5/6 xl:w-5/8 h-45 tracking-widest font-bold font-ranchers sm:text-left sm:leading-15 text-shadow-2xs text-shadow-black">
                <span className="text-esmerald">{firstWord}</span>{' '}
                {rest.join(' ')}
           </h1>
