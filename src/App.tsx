01ecdf206c3e62a507186bc5d0db19bb0f098a9d
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-01 19:46:44 -0300

    fix: desmembrando o Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import ContactUs from './components/ContactUs/ContactUs';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import Invite from './components/Invite/Invite';
 import LocaleWay from './components/LocaleWay/LocaleWay';
 import OurValues from './components/OurValue/OurValues';
@@ -12,6 +13,7 @@ import WeOffer from './components/WeOffer/WeOffer';
 function App() {
      return (
           <div className="flex flex-col items-center justify-between">
+               <Header />
                <Presentation />
                <LocaleWay />
                <OurValues />
