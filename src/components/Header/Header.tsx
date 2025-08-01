04436289d6028baff9bc6774582207b47785ba85
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-01 19:47:12 -0300

    fix: ajustando altura

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import { useRef, type ReactElement } from 'react';
+import logo from '@images/logo.svg';
+import NavBar from './NavBar';
+import { items } from '../../data/NavBarItems';
+import { useScroll } from 'src/hooks/useScroll';
+
+export default function Header(): ReactElement {
+     const headerRef = useRef<HTMLDivElement>(null);
+     useScroll(headerRef, 1.5, 0, -50);
+
+     return (
+          <header
+               ref={headerRef}
+               className="padding-h-content w-full h-16 lg:h-16 px-4 py-1 flex flex-row items-center justify-between gap-y-2 bg-esmerald">
+               <div className="w-28 h-12 md:w-fit md:h-10 flex-shrink-0 flex items-center justify-center">
+                    <img
+                         src={logo}
+                         alt="Logo"
+                         className="w-full h-full object-contain"
+                    />
+               </div>
+               <div className="w-fit lg:w-full flex justify-center lg:justify-end">
+                    <NavBar items={items} />
+               </div>
+          </header>
+     );
+}
