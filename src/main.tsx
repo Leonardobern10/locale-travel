535ba759aa0956f5a0449a151cf15c7eb29b5fc1
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-02 14:34:35 -0300

    add: definição das rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/Home.tsx';
+import App from './App.tsx';
+import Register from './pages/Register.tsx';
+import Login from './pages/Login.tsx';
 
 createRoot(document.getElementById('root')!).render(
      <StrictMode>
           <BrowserRouter>
                <Routes>
-                    <Route path="/" element={<App />} />
+                    <Route path="/" element={<App />}>
+                         <Route index element={<Home />} />
+                         <Route path="register" element={<Register />} />
+                         <Route path="login" element={<Login />} />
+                    </Route>
                </Routes>
           </BrowserRouter>
      </StrictMode>
