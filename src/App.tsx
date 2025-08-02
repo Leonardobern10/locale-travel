8f872020aa0a83afd1286dd55008790fa68769eb
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-02 14:34:16 -0300

    fix: nova disposição de conteudo utilizando Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
-import ContactUs from './components/ContactUs/ContactUs';
+import { Outlet } from 'react-router';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Invite from './components/Invite/Invite';
-import LocaleWay from './components/LocaleWay/LocaleWay';
-import OurValues from './components/OurValue/OurValues';
-import Presentation from './components/Presentation/Presentation';
-import ReadAbout from './components/ReadAbout/ReadAbout';
-import WeDo from './components/WeDo/WeDo';
-
-import WeOffer from './components/WeOffer/WeOffer';
 
 function App() {
      return (
           <div className="flex flex-col items-center justify-between">
                <Header />
-               <Presentation />
-               <LocaleWay />
-               <OurValues />
-               <WeOffer />
-               <WeDo />
-               <ReadAbout />
-               <ContactUs />
-               <Invite />
+               <Outlet />
                <Footer />
           </div>
      );
