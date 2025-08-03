414fd6a6f130f300b1c153d2586796c7c4ac57a7
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 12:35:03 -0300

    fix: formatando para utilizad Link para roteamento

--- src/components/LinkDefault.tsx
+++ src/components/LinkDefault.tsx
@@ -1,13 +1,20 @@
+import type { ButtonThemeType } from 'src/types/button/ButtonThemeType';
 import type { ReactElement } from 'react';
 import { FaArrowTrendUp } from 'react-icons/fa6';
+import { Link } from 'react-router';
 
-export default function LinkDefault(props: { linkName: string }): ReactElement {
+export default function LinkDefault(props: {
+     linkName: string;
+     icon: boolean;
+     theme: ButtonThemeType;
+     path: string;
+}): ReactElement {
      return (
-          <a
-               href=""
-               className="font-hunnin w-fit h-5 px-2 py-4 rounded-2xl text-esmerald last:mask-conic-to-neutral-50 mt-5 flex flex-row items-center justify-between gap-x-1 hover:text-white hover:bg-esmerald transition-colors delay-100">
+          <Link
+               to={props.path}
+               className={`font-hunnin w-fit h-5 px-2 py-4 rounded-2xl ${props.theme ?? 'hover:text-white hover:bg-esmerald text-esmerald last:mask-conic-to-neutral-50'}  mt-5 flex flex-row items-center justify-between gap-x-1 transition-colors delay-100`}>
                <p className="link">{props.linkName}</p>
-               <FaArrowTrendUp className="w-fit h-fit" />
-          </a>
+               {props.icon && <FaArrowTrendUp className="w-fit h-fit" />}
+          </Link>
      );
 }
