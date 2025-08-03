e427985298ad5784597957903307f6e53f815f91
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 11:56:44 -0300

    new: definição dos dados utilizados no Register

---
+++ src/data/RegisterData.ts
@@ -0,0 +1,14 @@
+import type { RegisterDataType } from 'src/types/RegisterDataType';
+
+export const RegisterData: RegisterDataType = {
+     backToHome: 'Página inicial',
+     titleImage: 'Cadastre-se agora!',
+     descriptionImage:
+          'Garante os melhores pacotes de viajens. Transformamos cada viagem em uma experiência única, feita com propósito e paixão',
+     rights: 'Locale Travel 2025. Todos os direitos reservados.',
+     terms: 'Terms of service',
+     titleForm: 'Cadastro',
+     descriptionForm:
+          'Bem-vindo à nossa plataforma. Insira seus dados para se cadastrar.',
+     buttonSubmit: 'Enviar'
+};
