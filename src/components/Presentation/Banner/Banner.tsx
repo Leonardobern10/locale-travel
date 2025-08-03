ff6c77eb91127b0c8e7e4fd89ccfd2a077655348
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 11:56:14 -0300

    add: inserindo componente DescriptionText

--- src/components/Presentation/Banner/Banner.tsx
+++ src/components/Presentation/Banner/Banner.tsx
@@ -3,6 +3,7 @@ import { useRef, type ReactElement } from 'react';
 import BannerTitle from './BannerTitle';
 import LinkDefault from 'src/components/LinkDefault';
 import { useScroll } from 'src/hooks/useScroll';
+import DescriptionText from 'src/components/DescriptionText';
 
 export default function Banner({
      bannerTitle,
@@ -17,9 +18,7 @@ export default function Banner({
                className="p-6 w-full lg:w-5/7 h-screen flex flex-col items-center lg:items-start justify-center lg:justify-center gap-y-4 md:gap-y-4">
                <BannerTitle title={bannerTitle} />
                <div>
-                    <p className="text-white w-full lg:w-5/7 font-hunnin text-md md:text-lg md:text-center lg:text-left">
-                         {description}
-                    </p>
+                    <DescriptionText description={description} />
                     <LinkDefault linkName={buttonContent} />
                </div>
           </div>
