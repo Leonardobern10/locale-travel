c7a2c6dbb5d7bc248ad91a3ba16ff59fd3c0929d
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 12:34:19 -0300

    new: temas pre-definidos para botao

--- src/types/button/ButtonThemeType.ts
+++ src/types/button/ButtonThemeType.ts
@@ -1,1 +1,5 @@
-export type ThemeButtonType = {};
+export enum ButtonThemeType {
+     BLACK = 'bg-black-default text-white',
+     WHITE = 'bg-white text-esmerald border-2 border-esmerald',
+     TRANSPARENT = 'bg-trasparent text-neutral-400 border-1 border-neutral-400 hover:text-neutral-200 hover:border-neutral-200'
+}
