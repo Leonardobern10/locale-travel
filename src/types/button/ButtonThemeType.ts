beed02906ef1bc8ce03bc6c365aab7dc9388741b
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 15:14:04 -0300

    add: definindo novo tema para o botao

--- src/types/button/ButtonThemeType.ts
+++ src/types/button/ButtonThemeType.ts
@@ -1,5 +1,6 @@
 export enum ButtonThemeType {
      BLACK = 'bg-black-default text-white',
      WHITE = 'bg-white text-esmerald border-2 border-esmerald',
-     TRANSPARENT = 'bg-trasparent text-neutral-400 border-1 border-neutral-400 hover:text-neutral-200 hover:border-neutral-200'
+     TRANSPARENT = 'bg-trasparent text-neutral-400 border-1 border-neutral-400 hover:text-neutral-200 hover:border-neutral-200',
+     BLUE = 'hover:text-white hover:bg-esmerald text-esmerald last:mask-conic-to-neutral-50'
 }
