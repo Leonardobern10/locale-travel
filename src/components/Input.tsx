a75bfe32e552ccf9333d6ea7b7b3ffa2660044f9
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 11:54:52 -0300

    fix: aplicação da classe que remove as setas

---
+++ src/components/Input.tsx
@@ -0,0 +1,34 @@
+import { InputType } from 'src/types/InputType';
+import type { ReactElement } from 'react';
+import type { UseFormRegisterReturn } from 'react-hook-form';
+
+export default function Input(props: {
+     email: InputType;
+     placeholder: string;
+     id: string;
+     value?: string;
+     register?: UseFormRegisterReturn;
+     required?: boolean;
+     onChange?: (
+          e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+     ) => void;
+}): ReactElement {
+     const handleOnChange = (
+          e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+     ) => {
+          e.stopPropagation();
+          props.onChange?.(e);
+     };
+
+     return (
+          <input
+               id={props.id}
+               className={`w-full h-13 border border-white/50 text-neutral-200 px-4 py-2 rounded-xl placeholder:text-neutral-600/50 focus:text-white focus:bg-white/50 focus:outline-none ${props.email === InputType.NUMBER ? 'input-no-spin' : ''}`}
+               type={props.email ?? 'text'}
+               placeholder={props.placeholder}
+               onChange={handleOnChange}
+               required={props.required ?? false}
+               {...props.register}
+          />
+     );
+}
