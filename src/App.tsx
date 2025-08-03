8492d8c8a76adeaa34b676414be86387a12d556d
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 11:51:59 -0300

    fix: removendo Outlet e inserindo o componente Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Outlet } from 'react-router';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Home from './pages/Home';
 
 function App() {
      return (
           <div className="flex flex-col items-center justify-between">
                <Header />
-               <Outlet />
+               <Home />
                <Footer />
           </div>
      );
