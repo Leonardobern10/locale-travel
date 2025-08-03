72f438230aa630c85810dda3777f23586aa933ac
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 15:12:48 -0300

    fix: criando o Logo como um Link para direcionar para a home

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,17 @@
 import type { ReactElement } from 'react';
 import logo from '@images/logo.svg';
+import { Link } from 'react-router';
 
 export default function Logo(): ReactElement {
      return (
-          <div className="w-28 h-12 md:w-fit md:h-10 flex-shrink-0 flex items-center justify-center">
-               <img
-                    src={logo}
-                    alt="Logo"
-                    className="w-full h-full object-contain"
-               />
+          <div className="w-28 h-12 md:w-30 md:h-10 flex-shrink-0 flex items-center justify-center hover:cursor-pointer">
+               <Link to="/" className="w-full h-full">
+                    <img
+                         src={logo}
+                         alt="Logo"
+                         className="w-full h-full object-contain"
+                    />
+               </Link>
           </div>
      );
 }
