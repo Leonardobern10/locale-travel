f898d2d01394ff3a525a3f9cacc5e7619d1fc36e
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 11:51:39 -0300

    fix: ajustando caminho relativo para @types

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-  },
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "./src"),
-      "@images": path.resolve(__dirname, "src/assets/img"),
-      "@data": path.resolve(__dirname, "src/data"),
-      "@types": path.relative(__dirname, "src/types"),
-      "@components": path.relative(__dirname, "src/components"),
-    },
-  },
+     plugins: [react(), tailwindcss()],
+     test: {
+          globals: true,
+          environment: 'jsdom',
+          setupFiles: './src/setupTests.ts'
+     },
+     resolve: {
+          alias: {
+               src: path.resolve(__dirname, './src'),
+               '@images': path.resolve(__dirname, 'src/assets/img'),
+               '@data': path.resolve(__dirname, 'src/data'),
+               '@types': path.relative(__dirname, 'src/types'),
+               '@components': path.relative(__dirname, 'src/components')
+          }
+     }
 });
