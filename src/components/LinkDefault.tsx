c873b619c9d8f8ea038e32eb3d5257e1718c9b9f
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-03 15:12:30 -0300

    fix: alterando padding

--- src/components/LinkDefault.tsx
+++ src/components/LinkDefault.tsx
@@ -7,12 +7,12 @@ export default function LinkDefault(props: {
      linkName: string;
      icon: boolean;
      theme: ButtonThemeType;
-     path: string;
+     path?: string;
 }): ReactElement {
      return (
           <Link
-               to={props.path}
-               className={`font-hunnin w-fit h-5 px-2 py-4 rounded-2xl ${props.theme ?? 'hover:text-white hover:bg-esmerald text-esmerald last:mask-conic-to-neutral-50'}  mt-5 flex flex-row items-center justify-between gap-x-1 transition-colors delay-100`}>
+               to={props.path ?? ''}
+               className={`font-hunnin w-fit h-5 px-3 py-4 rounded-2xl ${props.theme ?? 'hover:text-white hover:bg-esmerald text-esmerald last:mask-conic-to-neutral-50'}  mt-5 flex flex-row items-center justify-between gap-x-1 transition-colors delay-100`}>
                <p className="link">{props.linkName}</p>
                {props.icon && <FaArrowTrendUp className="w-fit h-fit" />}
           </Link>
