9734167b655828bfa4b209ef05be271a067607ed
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-04 16:18:37 -0300

    new: definição dos dados para registro

---
+++ src/types/user/RegisterDataType.ts
@@ -0,0 +1,12 @@
+export type RegisterDataType = {
+     backToHome: string;
+     titleImage: string;
+     descriptionImage: string;
+     rights: string;
+     terms: string;
+     titleForm: string;
+     descriptionForm: string;
+     buttonSubmit: string;
+     msgToastSuccess: string;
+     msgToastError: string;
+};
