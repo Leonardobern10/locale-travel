59caa59c1aab1fbe1604d4401bde189b259a07ba
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-04 16:11:01 -0300

    fix: alterando nome da props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,7 @@ import type { ReactElement } from 'react';
 import type { UseFormRegisterReturn } from 'react-hook-form';
 
 export default function Input(props: {
-     email: InputType;
+     type: InputType;
      placeholder: string;
      id: string;
      value?: string;
@@ -23,8 +23,8 @@ export default function Input(props: {
      return (
           <input
                id={props.id}
-               className={`w-full h-13 border border-white/50 text-neutral-200 px-4 py-2 rounded-xl placeholder:text-neutral-600/50 focus:text-white focus:bg-white/50 focus:outline-none ${props.email === InputType.NUMBER ? 'input-no-spin' : ''}`}
-               type={props.email ?? 'text'}
+               className={`w-full h-13 border border-white/50 text-neutral-200 px-4 py-2 rounded-xl placeholder:text-neutral-600/50 focus:text-white focus:bg-white/50 focus:outline-none ${props.type === InputType.NUMBER ? 'input-no-spin' : ''}`}
+               type={props.type ?? 'text'}
                placeholder={props.placeholder}
                onChange={handleOnChange}
                required={props.required ?? false}
