7b1ff0796eaf35f4e82394098796eed66dbb7805
Leonardobern <leonardo.bernardo2658@gmail.com>
2025-08-04 21:55:36 -0300

    feat: inserindo botaão para logout

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -4,7 +4,24 @@ import { Link } from 'react-scroll';
 import { Link as LinkRouter } from 'react-router';
 import type { NavBarProps } from 'src/types/navbar/NavBarProps';
 
+import { useAuthStore } from '@/store/useAuthStore';
+import ButtonHeader from '../Buttons/ButtonHeader';
+import { ButtonHeaderThemeType } from 'src/types/button/ButtonHeaderThemeType';
+
+import { logout as submitLogout } from 'src/services/UserService';
+
 export default function NavBar({ items }: NavBarProps): ReactElement {
+     const { user, isAuthenticated, logout } = useAuthStore();
+
+     const handleLogout = async () => {
+          try {
+               await submitLogout();
+               logout();
+          } catch (error) {
+               console.error(error);
+          }
+     };
+
      return (
           <nav className="w-fit flex flex-row items-center justify-between md:justify-between gap-x-40">
                {useWidth() && (
@@ -23,18 +40,31 @@ export default function NavBar({ items }: NavBarProps): ReactElement {
                          ))}
                     </ul>
                )}
-               <div className="flex flex-row gap-x-4 ">
-                    <LinkRouter
-                         to="/register"
-                         className="border border-white font-ibm px-3 py-1 rounded hover:bg-white hover:text-black transition">
-                         Cadastro
-                    </LinkRouter>
-                    <LinkRouter
-                         to="/login"
-                         className="border border-white font-ibm px-3 py-1 rounded hover:bg-white hover:text-black transition">
-                         Login
-                    </LinkRouter>
-               </div>
+               {!isAuthenticated ? (
+                    <div className="flex flex-row gap-x-4 ">
+                         <LinkRouter
+                              to="/register"
+                              className="border text-sm border-white font-ibm px-3 py-1 rounded hover:bg-white hover:text-black transition">
+                              Cadastro
+                         </LinkRouter>
+                         <LinkRouter
+                              to="/login"
+                              className="border text-sm border-white font-ibm px-3 py-1 rounded hover:bg-white hover:text-black transition">
+                              Login
+                         </LinkRouter>
+                    </div>
+               ) : (
+                    <div className="flex flex-row items-center gap-x-4">
+                         <p className="font-light">
+                              Bem vindo, {`${user!.nome}`}!
+                         </p>
+                         <ButtonHeader
+                              theme={ButtonHeaderThemeType.EXIT}
+                              title="Sair"
+                              event={handleLogout}
+                         />
+                    </div>
+               )}
           </nav>
      );
 }
